b711aea19ceef076072567648c3d5160a7e1c141
arin7al <2522634github>
2020-09-17 20:56:00 +0300

    added plot checking which neurons are poisson

--- quiz4/quiz4.py
+++ quiz4/quiz4.py
@@ -2,9 +2,32 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
+def meanSpikeAndVarianceCount(neuron_name, data):
+    num_of_spikes = data[neuron_name] * 10
+    num_of_experiments = len(num_of_spikes)
+    mean = np.sum(num_of_spikes, axis=0) / num_of_experiments
+    variance = np.sum((num_of_spikes - np.array([mean, ] * num_of_experiments)) ** 2, axis=0) / (num_of_experiments - 1)
+    return mean, variance
+
 with open('tuning_2.7.pickle', 'rb') as f:
     data = pickle.load(f)
     # plotting the tuning curve
-    plt.plot(data['stim'], np.sum(data['neuron1'], axis=0))
-    plt.show()
+    plot1 = plt.figure(1)
+    plt.plot(data['stim'], np.sum(data['neuron1'], axis=0)/len(data['neuron1']))
+    plt.xlabel('stimulus')
+    plt.ylabel('mean firing rate of neuron')
 
+    plot2 = plt.figure(2)
+    (mean, variance) = meanSpikeAndVarianceCount('neuron1',data)
+    plt.plot(mean, variance, 'bo', label='Neuron1')
+    (mean, variance) = meanSpikeAndVarianceCount('neuron2',data)
+    plt.plot(mean, variance, 'ro', label='Neuron2')
+    (mean, variance) = meanSpikeAndVarianceCount('neuron3',data)
+    plt.plot(mean, variance, 'go', label='Neuron3')
+    (mean, variance) = meanSpikeAndVarianceCount('neuron4',data)
+    plt.plot(mean, variance, 'yo', label='Neuron4')
+    plt.plot(np.linspace(0, 350), np.linspace(0, 350), label='Fano factor of poisson process')
+    plt.xlabel('mean number of spikes')
+    plt.ylabel('variance in number of spikes')
+    plt.legend()
+    plt.show()
